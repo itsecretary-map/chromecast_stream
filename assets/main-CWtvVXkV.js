cfc98518b0ce840cccbbe8972f14cef56686da1d
Abdullah Al-Nayeem <nayeem@Abdullahs-MBP.lan>
2025-08-15 17:02:07 -0400

    Updates

--- assets/main-CWtvVXkV.js
+++ assets/main-CWtvVXkV.js
@@ -671,7 +671,6 @@ function renderQrCodes() {
             switch(key) {
               case 'mapWebsite': return 'mapitt.png';
               case 'communityWhatsApp': return 'whatsapp_group.png';
-              case 'donation': return 'mohid_donation.png';
               default: return 'mapitt.png';
             }
           };
